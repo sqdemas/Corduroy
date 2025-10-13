55a9bc9a35854d4f97d6206cbaef2396f627de9c
samde <samdemas24@gmail.com>
2025-10-13 16:00:57 -0400

    updated corduroy asset

--- Assets/_Scripts/ObstacleMover.cs
+++ Assets/_Scripts/ObstacleMover.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public int minSpeed = 1;
-    public int maxSpeed = 10;
-    private int speed;
+    public float speed = 0.5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // float growScale = Random.Range(1f, 4f);
-        speed = Random.Range(minSpeed, maxSpeed);
-        // transform.localScale += Vector3.one * growScale;
+        // transform.localPosition.x += Vector3.one * growScale;
     }
 
     // Update is called once per frame
